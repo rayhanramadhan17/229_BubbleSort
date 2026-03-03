2a3ae266738e7d639792ef1fce8ba8fa34c9dafe
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-03 11:24:03 +0700

    add deklarasi

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,1 +1,7 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variable global array a dengan ukuran 20
+int n;       // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+
