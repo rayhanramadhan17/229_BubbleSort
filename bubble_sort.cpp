decdf8a4faae4a3fc8c437816a977c9c436ab604
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-03 11:30:15 +0700

    looping,prosedur mengurutkan array

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -25,4 +25,38 @@ void input()
     cout << "Masukan Elemen Array" << endl;
     cout << "=====================" << endl;
 
-  
+    for (int i = 0; i < n; i++) // Looping dengan i dimulai dari 0 hingga n-1
+    {
+        cout << "Data ke-" << (i + 1 ) << ": ";
+        cin >> arr[i];
+    }
+}
+void bubbleSortArray()
+{
+    // procedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1; // step 1
+
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        { // step 2
+            if (arr[j] > arr[j + 1])
+            { // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+        pass += 1; // step 4
+
+        cout << "\nPass " << pass - 1 << ": "; // Number of pass
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; // Menampilkan data pada number of pass
+        }
+        cout << endl;
+
+    } while (pass <= n - 1); // step 5
+}
